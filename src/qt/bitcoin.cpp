6b39799842fcf82a2025c172c2014ae4a6a57df0
e-nexus <34931578+e-nexus@users.noreply.github.com>
2018-01-28 05:00:02 +1000

    Update bitcoin.cpp

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -50,7 +50,6 @@ Q_IMPORT_PLUGIN(qtwcodecs)
 Q_IMPORT_PLUGIN(qkrcodecs)
 Q_IMPORT_PLUGIN(qtaccessiblewidgets)
 #else
-Q_IMPORT_PLUGIN(AccessibleFactory)
 Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
 #endif
 #endif
